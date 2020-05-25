751a6db551cf79f549c194134c8b612f316627b3
Dipak Parmar <hi@dipak.tech>
2020-05-25 02:43:10 -0700

    hide sponsered advts.

--- customize-mail.js
+++ customize-mail.js
@@ -1,5 +1,16 @@
-window.onload = function() {
-    if (window.location.href.indexOf("#inbox") == -1) {
-        return;
-    }
-}
+window.onload = function () {
+  if (window.location.href.indexOf("#inbox") == -1) {
+    return;
+  }
+
+  setInterval(function () {
+
+    //hide sponsered advts.
+    Array.from(document.getElementsByClassName("zA zE")).forEach(function (
+      labelPill
+    ) {
+      labelPill.style.display = "none";
+    }),
+      500;
+  });
+};
